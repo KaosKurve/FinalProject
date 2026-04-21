f31f185f095641617cab453ba6eefddb9f984749
Kaoskurve <Kaoskurve@gmail.com>
2026-04-20 21:42:59 -0500

    set up basic window display

--- src/paintingapp.py
+++ src/paintingapp.py
@@ -6,10 +6,19 @@ timer = pygame.time.Clock()
 Width = 800
 Height = 600
 
+
 screen = pygame.display.set_mode([Width, Height])
 pygame.display.set_caption('Paint!')
+
 
 run = True
 while run:
     timer.tick(fps)
     screen.fill('white')
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.flip()
+pygame.quit()
