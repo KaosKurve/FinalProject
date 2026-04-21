9e6f3eae37e177f7807b60415c2a87b55f012bce
Kaoskurve <Kaoskurve@gmail.com>
2026-04-20 22:12:16 -0500

    set-up events to track brush sizes and colors

--- src/paintingapp.py
+++ src/paintingapp.py
@@ -5,7 +5,8 @@ fps = 60
 timer = pygame.time.Clock()
 Width = 800
 Height = 600
-
+active_size = 0
+active_color = 'white'
 
 screen = pygame.display.set_mode([Width, Height])
 pygame.display.set_caption('Paint!')
@@ -22,6 +23,7 @@ def draw_menu():
     pygame.draw.circle(screen, 'white', (155, 35), 10)
     s_brush = pygame.draw.rect(screen, 'black', [190, 10, 50, 50])
     pygame.draw.circle(screen, 'white', (215, 35), 5)
+    brush_list = [xl_brush, l_brush, m_brush, s_brush]
 
     blue = pygame.draw.rect(screen, (0, 0, 255), [Width - 35, 10, 25, 25])
     red = pygame.draw.rect(screen, (255, 0, 0), [Width - 35, 35, 25, 25])
@@ -31,19 +33,31 @@ def draw_menu():
     purple = pygame.draw.rect(screen, (255, 0, 255), [Width - 85, 35, 25, 25])
     white = pygame.draw.rect(screen, (0, 0, 0), [Width - 110, 10, 25, 25])
     black = pygame.draw.rect(screen, (255, 255, 255), [Width - 110, 35, 25, 25])
-
+    color_rect = [blue, red, green, yellow, teal, purple, white, black]
+    rgb_list = [(0, 0, 255), (255, 0, 0), (0, 255, 0), (255, 255, 0),
+                (0, 255, 255), (255, 0, 255),(0, 0, 0), (255, 255, 255)]
+    return brush_list, color_rect, rgb_list
 
 
 run = True
 while run:
     timer.tick(fps)
     screen.fill('white')
-
-    draw_menu()
+    mouse = pygame.mouse.get_pos()
+    brushes, colors, rgbs = draw_menu()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for i in range(len(brushes)):
+                if brushes[i].collidepoint(event.pos):
+                    active_size = 20 - (i * 5)
+            
+            for i in range(len(colors)):
+                if colors[i].collidepoint(event.pos):
+                    active_color = rgbs[i]
 
     pygame.display.flip()
 pygame.quit()
