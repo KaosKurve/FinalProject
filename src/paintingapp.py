fe2c7795ed0d438452efc35a00fa799d9912a84d
Kaoskurve <Kaoskurve@gmail.com>
2026-04-20 22:47:34 -0500

    indicate what brush/color selected, made it so cursor is above the paint

--- src/paintingapp.py
+++ src/paintingapp.py
@@ -1,7 +1,7 @@
 import pygame
 pygame.init()
 
-fps = 60
+fps = 240
 timer = pygame.time.Clock()
 Width = 800
 Height = 600
@@ -12,9 +12,10 @@ pygame.display.set_caption('Paint!')
 painting = []
 
 
-def draw_menu():
+def draw_menu(size, color):
     pygame.draw.rect(screen, 'gray', [0, 0 , Width, 70])
     pygame.draw.line(screen, 'black', (0, 70), (Width, 70), 3)
+
     xl_brush = pygame.draw.rect(screen, 'black', [10, 10, 50, 50])
     pygame.draw.circle(screen, 'white', (35, 35), 20)
     l_brush = pygame.draw.rect(screen, 'black', [70, 10, 50, 50])
@@ -24,6 +25,17 @@ def draw_menu():
     s_brush = pygame.draw.rect(screen, 'black', [190, 10, 50, 50])
     pygame.draw.circle(screen, 'white', (215, 35), 5)
     brush_list = [xl_brush, l_brush, m_brush, s_brush]
+    if size == 20:
+        pygame.draw.rect(screen, 'green', [10, 10, 50, 50], 3)
+    if size == 15:
+        pygame.draw.rect(screen, 'green', [70, 10, 50, 50], 3)
+    if size == 10:
+        pygame.draw.rect(screen, 'green', [130, 10, 50, 50], 3)
+    if size == 5:
+        pygame.draw.rect(screen, 'green', [190, 10, 50, 50], 3)
+
+    pygame.draw.circle(screen, color, (400, 35), 30)
+    pygame.draw.circle(screen, 'dark grey', (400, 35), 30, 3)
 
     blue = pygame.draw.rect(screen, (0, 0, 255), [Width - 35, 10, 25, 25])
     red = pygame.draw.rect(screen, (255, 0, 0), [Width - 35, 35, 25, 25])
@@ -50,13 +62,13 @@ while run:
     screen.fill('white')
     mouse = pygame.mouse.get_pos()
     left_click = pygame.mouse.get_pressed()[0]
-    if mouse[1] > 70:
-        pygame.draw.circle(screen, active_color, mouse, active_size)
+
     if left_click and mouse[1] > 70:
         painting.append((active_color, mouse, active_size))
     draw_painting(painting)
-    
-    brushes, colors, rgbs = draw_menu()
+    if mouse[1] > 70:
+        pygame.draw.circle(screen, active_color, mouse, active_size)
+    brushes, colors, rgbs = draw_menu(active_size, active_color)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
